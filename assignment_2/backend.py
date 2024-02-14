1505c50aa9ce6dc219e5c0d90aa15ae6a798daf9
anjalis2112 <singhanj@usc.edu>
2024-02-14 01:28:22 -0800

    fixed charts and formatting

--- assignment_2/backend.py
+++ assignment_2/backend.py
@@ -10,6 +10,7 @@ CORS(app)
 API_KEY = "cn4rrc1r01qgb8m674g0cn4rrc1r01qgb8m674gg"
 API_KEY_POLYGON = "mtPTlyDLSn3iRxUVJsclL6rB3ZvHUyh0"
 
+
 @app.route('/stock/profile', methods=['GET'])
 def get_stock_profile():
     symbol = request.args.get('symbol')
@@ -24,6 +25,7 @@ def get_stock_profile():
             return jsonify({'error': 'Failed to fetch data from Finnhub API'})
     else:
         return jsonify({'error': 'Symbol parameter is missing'})
+
 
 @app.route('/stock/summary', methods=['GET'])
 def get_stock_summary():
@@ -40,6 +42,7 @@ def get_stock_summary():
     else:
         return jsonify({'error': 'Symbol parameter is missing'})
 
+
 @app.route('/stock/recommendation-trends', methods=['GET'])
 def get_recommendation_trends():
     symbol = request.args.get('symbol')
@@ -55,6 +58,7 @@ def get_recommendation_trends():
     else:
         return jsonify({'error': 'Symbol parameter is missing'})
 
+
 @app.route('/stock/company-news', methods=['GET'])
 def get_company_news():
     symbol = request.args.get('symbol')
@@ -62,7 +66,7 @@ def get_company_news():
     if symbol:
         thirty_days_ago = datetime.now() - relativedelta(days=30)
         date_thirty_days_ago = thirty_days_ago.strftime("%Y-%m-%d")
-        
+
         current_date = datetime.now().strftime("%Y-%m-%d")
         url = f"https://finnhub.io/api/v1/company-news?symbol={symbol}&from={date_thirty_days_ago}&to={current_date}&token={API_KEY}"
         response = requests.get(url)
@@ -73,7 +77,8 @@ def get_company_news():
             return jsonify({'error': 'Failed to fetch data from Finnhub API'})
     else:
         return jsonify({'error': 'Symbol parameter is missing'})
-    
+
+
 @app.route('/stock/chart-data', methods=['GET'])
 def get_chart_data():
     symbol = request.args.get('symbol')
@@ -82,19 +87,22 @@ def get_chart_data():
         multiplier = 1
         timespan = "day"
         six_months_one_day_ago = datetime.now() - relativedelta(months=6, days=1)
-        date_six_months_one_day_ago = six_months_one_day_ago.strftime("%Y-%m-%d")
+        date_six_months_one_day_ago = six_months_one_day_ago.strftime(
+            "%Y-%m-%d")
         current_date = datetime.now().strftime("%Y-%m-%d")
         added_string = "adjusted=true&sort=asc&apiKey=" + API_KEY_POLYGON
         url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{date_six_months_one_day_ago}/{current_date}?{added_string}"
         response = requests.get(url)
 
         if response.status_code == 200:
-            print(response.json())
+            print(date_six_months_one_day_ago)
+            print(current_date)
             return response.json()
         else:
             return jsonify({'error': 'Failed to fetch data from Polygon API'})
     else:
         return jsonify({'error': 'Symbol parameter is missing'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
